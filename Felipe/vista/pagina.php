2ccee6935e27d12e257da6faff5febcb9aa0689d
FelipeRivera821 <133450895+FelipeRivera821@users.noreply.github.com>
2023-05-25 16:52:51 -0600

    Add files via upload

---
+++ Felipe/vista/pagina.php
@@ -0,0 +1,8 @@
+<?php
+$categoria = $_GET['categoria'];
+$producto = $_GET['producto'];
+
+echo "Selecciono el producto " . $producto . " de la categoría ". $categoria;
+
+// Estos nos imprimirá en el navegador -> Selecciono el producto 3 de la categoría ropa
+?>
---
+++ Felipe/vista/pagina2.php
@@ -0,0 +1,13 @@
+<a href="http://localhost/pagina.php?categoria=ropa&producto=3">ver</a>
+<form action=”pagina.php” method=”GET” >
+Categoria: <input type=”text” name=”name” />
+Producto:
+<select name=”producto” >
+<option value=”1″> Polo </option>
+<option value=”2″> Quiksilver </option>
+<option value=”3″> Tommy </option>
+</select >
+<input type=”submit” value= ”Enviar” />
+
+</form >
+
